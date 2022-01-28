61bbe2f59f51cc8ab3a787566b0aaa0ab2a45cb8
Shicong Wang <sw89@pyrite.clear.rice.edu>
2022-01-27 21:34:39 -0600

    Modified Pages and renew the methods

--- A1/A1/Main/BufferMgr/headers/MyDB_Page.h
+++ A1/A1/Main/BufferMgr/headers/MyDB_Page.h
@@ -1,10 +1,12 @@
 #ifndef PAGE_H
 #define PAGE_H
+
+#include "../../Catelog/headers/MyDB_Table.h"
 #include <string>
 #include <memory>
 
 //page are building blocks for buffer accessed by handler
-//property includes: 
+//property includeds: 
 //pinned, dirty, byte, refcount, temp?->to a single file
  using namespace std;
 class MyDB_Page;
@@ -14,7 +16,7 @@ class MyDB_PageHandleBase {
 public:
 
     // Constructure
-    MyDB_Page(MyDB_lookTablePtr lookTable, MyDB_BufferManager &pos,size_t offset); // TODO: Arguments remain to be checked
+    MyDB_Page(MyDB_TablePtr lookUpTable, MyDB_BufferManager &pos, size_t offset); // TODO: Arguments remain to be checked
     
     // Destructor
     ~MyDB_Page ();
@@ -25,22 +27,43 @@ public:
     // Set page to a dirty page if it is
 	void setDirty ();
 
+    // Increase the number of the PageHandleBase to the Page Object
     void incRef();
+    // Decrease the number of the PageHandleBase to the Page Object
     void decRef();
-    void setBytes();
+
+    // Set the bytes
+    void setBytes(void* byteVal);
+
+    // Check the page is dirty or not
     bool isDirty();
+
+    // Check the page is pinned or not
     bool isPinned();
+
+    // Pin the page
+    void setPin(bool pinned);
+
+    // Write the bytes into the page.
+    // TODO: Does this need to be change to pointer to call? 
+    void wroteBytes();
+
 
 	// FEEL FREE TO ADD ADDITIONAL PUBLIC METHODS
 
 private:
+
+    // Link to the buffer pool
     MyDB_BufferManager &pos;
-    MyDB_lookTablePtr lookTable;
+
+    // The look table
+    MyDB_TablePtr lookUpTable;
+
     bool dirty;
     bool pin;
     void *bytes;
     int ref;
-    int offset;
+    size_t offset;
 
 	// YOUR CODE HERE
 };
