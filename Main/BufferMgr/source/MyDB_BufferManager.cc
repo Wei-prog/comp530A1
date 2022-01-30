48b75fb8647e5ac66d89ecc168c34a90bec1b6f4
Shicong Wang <sw89@agate.clear.rice.edu>
2022-01-30 13:52:59 -0600

    Update

--- Main/BufferMgr/source/MyDB_BufferManager.cc
+++ Main/BufferMgr/source/MyDB_BufferManager.cc
@@ -9,35 +9,38 @@
 #include <stdlib.h>
 #include <iostream>
 #include <fstream>
+#include <map>
+#include <utility>
 
 using namespace std;
 
 //constructor, we need pageSize, numPages in buffer, and temp location
 //we want to create memory buffer, LRU list and an empty lookup table
-MyDB_PageHandle MyDB_BufferManager:: getPage(MyDB_TablePtr whichTable, long i){	
-	// size_t page_size,size_t numPages,string tempFile
-	if(whichTable == nullptr){
-		exit(1);
-	}
+MyDB_BufferManager:: MyDB_BufferManager(size_t page_size,size_t numPages,string tempFile){	
 
 	this->page_size = page_size;
 	this->numPages = numPages;
 	this->tempFile = tempFile;
-	file.open(tempFile,fstream::out)
+	// file.open(tempFile,fstream::out)
 	for (size_t i =0 ; i<numPages;i++){
 		this->memBuffer.push_back((void*)malloc(page_size));
 	}
 	//TODO: there is no need to initialize look up table or is there?
-	this->lookupTable=std::map<pair<MyDB_TablePtr,long>, MyDB_Pageptr>();
+	this->lookupTable=std::map<pair<MyDB_TablePtr,long >, MyDB_Pageptr>();
 	this->LRU = new LRUCache();
 
 }
 
 
 MyDB_BufferManager :: ~MyDB_BufferManager () {
+	// Empty the lookupTable
+	// for(auto page: this->lookupTable){
+
+	// }
+
 	//empty buffer
 	for(auto buf:this->memBuffer){
-		//write content to disk using LRU OR memBuffer
+		//write content to disk using LRU OR memBuffer TODO: ????
 		buf.writeToDisk()
 		free(buf);
 	}
@@ -61,18 +64,18 @@ MyDB_PageHandle MyDB_BufferManager :: getPage (MyDB_TablePtr whichTable, long i)
 	pair<MyDB_TablePtr,long> pageId = make_pair(whichTable,i);
 	//if no such page found, create and push into table
 	//see iterator find for iterating map
-	std::map<pair<MyDB_TablePtr,long>>::iterator it;
-	it=lookupTable.find(pageId);
-    if(it==lookupTable.end()){
-		if (this->currLeftMem==0)
+	map<pair<MyDB_TablePtr,long>,MyDB_Pageptr>::iterator it;
+	it=this->lookupTable.find(pageId);
+  if(it==this->lookupTable.end()){
+		if (this->currLeftMem==0){
 			this->evictPage();
+		}
 		MyDB_Pageptr naPage = make_shared<MyDB_Page> (whichTable,*this,i);
 		(this->LRU)->updateList(naPage);
 		return make_shared<MyDB_PageHandleBase> (naPage);
-	}
-	else{
+	}else{
 		MyDB_Pageptr naPage = lookupTable[pageId]
-		(this->LRU)->addPage(naPage);
+		(this->LRU)->addPage(naPage); // TODO: Check the LRU function
 		return make_shared<MyDB_PageHandleBase>(lookupTable[pageId]);
 	}//if found return from table
 	return nullptr;		
